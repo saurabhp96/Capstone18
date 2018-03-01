8f74af993adbe3473e4983fed8a4796747cf40d9
saurabhp96 <saurabhp96@gmail.com>
2018-03-01 10:38:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/capstone18/capstone/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.capstone18.capstone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button recipeButton=findViewById(R.id.recipesButton);
+        Button pantryButton=findViewById(R.id.pantryButton);
+
+        recipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent recipeIntent=new Intent(MainActivity.this,Recipes.class);
+                startActivity(recipeIntent);
+
+            }
+        });
+
+        pantryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pantryIntent=new Intent(MainActivity.this,Pantry.class);
+                startActivity(pantryIntent);
+            }
+        });
+
+    }
+}
