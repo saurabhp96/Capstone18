d3cee845fde0e79d2bed8227a0bff1177d9031b2
saurabhp96 <saurabhp96@gmail.com>
2018-03-07 14:22:13 -0500

    Did some work for pantry list activity

--- app/src/main/java/com/example/capstone18/capstone/Pantry.java
+++ app/src/main/java/com/example/capstone18/capstone/Pantry.java
@@ -1,13 +1,90 @@
 package com.example.capstone18.capstone;
 
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.io.File;
 
 public class Pantry extends AppCompatActivity {
+
+    ListView pantryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantry);
+        pantryList=(ListView)findViewById(R.id.pantry_list);
+
+        File file=new File(getFilesDir(),"pantry.txt");
+
+        ListAdapter adapter=new ListAdapter() {
+            @Override
+            public boolean areAllItemsEnabled() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled(int i) {
+                return false;
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                return null;
+            }
+
+            @Override
+            public int getItemViewType(int i) {
+                return 0;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        };
+        pantryList.setAdapter(adapter);
+
+
     }
 }
