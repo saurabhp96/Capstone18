4d98c90f9f651ea68c17247ff9c475ba7e21e37a
saurabhp96 <saurabhp96@gmail.com>
2018-03-30 23:11:15 -0400

    Added radio buttons for cuisine restriction on recipe options

--- app/src/main/java/com/example/capstone18/capstone/RecipeOptions.java
+++ app/src/main/java/com/example/capstone18/capstone/RecipeOptions.java
@@ -1,13 +1,17 @@
 package com.example.capstone18.capstone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.RadioGroup;
 
 public class RecipeOptions extends AppCompatActivity {
 
     private GridView gridView;
+    private RadioGroup restrictionGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,19 @@ public class RecipeOptions extends AppCompatActivity {
         String[] cuisines=getResources().getStringArray(R.array.cuisines);
         ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_multiple_choice,cuisines);
         gridView.setAdapter(adapter);
+        restrictionGroup=(RadioGroup)findViewById(R.id.cuisine_restriction);
 
 
+    }
+
+    public void okPressed(View v){
+        Bundle bundle=new Bundle();
+        Intent intent=new Intent(this,Recipes.class);
+        startActivity(intent,bundle);
+
+    }
+
+    public void cancelPressed(View v){
+        finish();
     }
 }
