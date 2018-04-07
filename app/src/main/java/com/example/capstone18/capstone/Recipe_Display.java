458ded0a02be7f7d0b197450101cfc2aa6b71a57
saurabhp96 <saurabhp96@gmail.com>
2018-04-07 17:15:36 -0400

    Worked on Recipe Display, loading image and injecting recipe info into the layout.

--- app/src/main/java/com/example/capstone18/capstone/Recipe_Display.java
+++ app/src/main/java/com/example/capstone18/capstone/Recipe_Display.java
@@ -1,5 +1,8 @@
 package com.example.capstone18.capstone;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +10,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Recipe_Display extends AppCompatActivity {
+    public static final String IMAGE_URL="url";
+    public static final String RECIPE_NAME="name";
+    public static final String INSTRUCTIONS="instructions";
+
     private TextView recipe_name,recipe_instructions;
     private ImageView recipe_image;
 
@@ -17,19 +28,57 @@ public class Recipe_Display extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe__display);
 
-        Toolbar toolbar=(Toolbar)findViewById(R.id.my_toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
-        ActionBar actionBar=getSupportActionBar();
+        ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        recipe_image=(ImageView)findViewById(R.id.recipeImage);
-        recipe_name=(TextView)findViewById(R.id.recipe_name);
-        recipe_instructions=(TextView)findViewById(R.id.recipe_instructions);
+        recipe_image = (ImageView) findViewById(R.id.recipeImage);
+        recipe_name = (TextView) findViewById(R.id.recipe_name);
+        recipe_instructions = (TextView) findViewById(R.id.recipe_instructions);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle == null) {
+            Toast.makeText(this, "Failed to get recipe info from Recipes list", Toast.LENGTH_LONG).show();
+            finishActivity(RESULT_CANCELED);
+        }
+
+        new ImageLoader(recipe_image).execute(bundle.getString(IMAGE_URL));
+        recipe_name.setText(bundle.getString(RECIPE_NAME));
+        recipe_instructions.setText(bundle.getString(INSTRUCTIONS));
 
 
     }
 
-    public void okPressed(View v){
+    private class ImageLoader extends AsyncTask<String,Void,Bitmap>{
+        ImageView imageView;
+
+        public ImageLoader(ImageView i){
+            imageView=i;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String url=urls[0];
+            Bitmap image=null;
+            try {
+                InputStream stream=new java.net.URL(url).openStream();
+                image= BitmapFactory.decodeStream(stream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return image;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    public void okPressed(View v) {
+        finishActivity(RESULT_OK);
 
     }
 
